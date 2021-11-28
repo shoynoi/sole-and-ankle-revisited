b86b75f593dcd41d49dc845b6dc2b42871d16b03
shoynoi <shoynoi.jp@gmail.com>
2021-11-28 14:59:30 +0900

    exercise 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-import { COLORS, WEIGHTS } from '../../constants';
+import { COLORS, QUERIES, WEIGHTS } from '../../constants';
 import Logo from '../Logo';
 import SuperHeader from '../SuperHeader';
 import MobileMenu from '../MobileMenu';
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
 
 const Header = () => {
   const [showMobileMenu, setShowMobileMenu] = React.useState(false);
@@ -29,7 +31,19 @@ const Header = () => {
           <NavLink href="/kids">Kids</NavLink>
           <NavLink href="/collections">Collections</NavLink>
         </Nav>
-        <Side />
+        <Side>
+          <MobileNav>
+            <UnstyledButton>
+              <Icon id="shopping-bag" strokeWidth={2} />
+            </UnstyledButton>
+            <UnstyledButton>
+              <Icon id="search" strokeWidth={2} />
+            </UnstyledButton>
+            <UnstyledButton>
+              <Icon id="menu" strokeWidth={2} />
+            </UnstyledButton>
+          </MobileNav>
+        </Side>
       </MainHeader>
 
       <MobileMenu
@@ -46,16 +60,29 @@ const MainHeader = styled.div`
   padding: 18px 32px;
   height: 72px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+
+  @media ${QUERIES.tabletAndSmaller} {
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
 
 const Nav = styled.nav`
   display: flex;
   gap: 48px;
   margin: 0px 48px;
+  
+  @media ${QUERIES.tabletAndSmaller} {
+    display: none;
+  }
 `;
 
 const Side = styled.div`
   flex: 1;
+  
+  @media ${QUERIES.tabletAndSmaller} {
+    flex: revert;
+  }
 `;
 
 const NavLink = styled.a`
@@ -69,5 +96,19 @@ const NavLink = styled.a`
     color: ${COLORS.secondary};
   }
 `;
+
+const MobileNav = styled.div`
+  display: none;
+  
+  @media ${QUERIES.tabletAndSmaller} {
+    display: flex;
+    gap: 40px;
+  }
+
+  @media ${QUERIES.phoneAndSmaller} {
+    display: flex;
+    gap: 20px;
+  }
+`
 
 export default Header;
